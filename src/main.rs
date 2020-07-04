45446046374e1b7f49eba116ff4ed7fe167ed333
Hunter Haydel <haydh530@gmail.com>
2020-07-03 21:45:18 -0400

    Copy sample code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
-fn main() {
-    println!("Hello, world!");
+use ggez::event;
+use ggez::graphics;
+use ggez::nalgebra as na;
+use ggez::{Context, ContextBuilder, GameResult};
+
+struct GameState {
+    pos_x: f32,
+}
+
+impl GameState {
+    fn new() -> GameResult<GameState> {
+        let s = GameState { pos_x: 0.0 };
+        Ok(s)
+    }
+}
+
+impl event::EventHandler for GameState {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult {
+        self.pos_x = self.pos_x % 800.0 + 1.0;
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult {
+        graphics::clear(ctx, [0.1, 0.2, 0.3, 1.0].into());
+
+        let circle = graphics::Mesh::new_circle(
+            ctx,
+            graphics::DrawMode::fill(),
+            na::Point2::new(self.pos_x, 380.0),
+            100.0,
+            2.0,
+            graphics::WHITE,
+        )?;
+        graphics::draw(ctx, &circle, (na::Point2::new(0.0, 0.0),))?;
+
+        graphics::present(ctx)?;
+        Ok(())
+    }
+}
+
+pub fn main() -> GameResult {
+    let cb = ContextBuilder::new("super_space_shape_battle", "wedgex");
+    let (ctx, event_loop) = &mut cb.build()?;
+    let state = &mut GameState::new()?;
+    event::run(ctx, event_loop, state)
 }
